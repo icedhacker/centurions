0c36c54a3a007a4cf008abf552efca58512107b9
icedhacker <mail2iceman@gmail.com>
2018-03-08 20:53:45 +0530

    Adding ProtoBuf support and basic spring boot application setup.

---
+++ src/main/java/com/centurions/CenturionsApplication.java
@@ -0,0 +1,11 @@
+package com.centurions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenturionsApplication {
+    public static void main (String[] args) {
+        SpringApplication.run(CenturionsApplication.class, args);
+    }
+}
