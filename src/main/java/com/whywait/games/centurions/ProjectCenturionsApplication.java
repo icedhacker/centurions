b62efd0c9d5b849e71f5913e6753694dac0f5cea
icedhacker <mail2iceman@gmail.com>
2018-06-27 22:04:37 +0800

    Initial Spring Boot Setup for Project Centurions

---
+++ src/main/java/com/whywait/games/centurions/ProjectCenturionsApplication.java
@@ -0,0 +1,12 @@
+package com.whywait.games.centurions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCenturionsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectCenturionsApplication.class, args);
+  }
+}
